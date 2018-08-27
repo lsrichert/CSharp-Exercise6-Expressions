5bf4a16ef0350fb5c882f5c5ccc2adb5f4cbe2b5
Lauren Richert <lsrichert@gmail.com>
2018-08-27 10:36:48 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise6Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
